3b226a5ca54e624388b013c8b2caa592ad00daac
root <root@ip-172-31-89-172.ec2.internal>
2020-01-12 03:25:55 +0000

    commiting maven project to here

---
+++ SampleJarProject/src/main/java/ashu/reddy/raj/App.java
@@ -0,0 +1,13 @@
+package ashu.reddy.raj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Devops!" );
+    }
+}
